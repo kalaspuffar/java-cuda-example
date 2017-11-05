d720385a18ecd5c5ad3df57899dc2ab1b98885b5
Daniel Persson <mailto.woden@gmail.com>
2017-11-05 20:31:36 +0100

    Added threaded example

---
+++ src/main/java/org/ea/gpu/JavaFilterMulti.java
@@ -0,0 +1,71 @@
+package org.ea.gpu;
+
+import javax.imageio.ImageIO;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class JavaFilterMulti {
+    private static final int[] filterDelute = new int[] {
+            1, 2, 1,
+            2, 4, 2,
+            1, 2, 1
+    };
+
+    public static void main(String[] args) {
+        try {
+
+            ExecutorService fixedPool = Executors.newFixedThreadPool(10);
+
+            BufferedImage bi = ImageIO.read(new File("test.png"));
+            int w = bi.getWidth();
+            int h = bi.getHeight();
+            int[] pixels = new int[w * h];
+
+            BufferedImage grayscaleImg = new BufferedImage(w, h, BufferedImage.TYPE_BYTE_GRAY);
+            Graphics g = grayscaleImg.getGraphics();
+            g.drawImage(bi, 0, 0, null);
+            g.dispose();
+
+            ImageIO.write(grayscaleImg, "PNG", new File("before.png"));
+
+            pixels = grayscaleImg.getRaster().getPixels(0, 0, w, h, pixels);
+
+            long start = System.currentTimeMillis();
+
+            int part = h / 10;
+
+            CompletionService completionService =
+                    new ExecutorCompletionService(fixedPool);
+
+            int[] resultVertical = new int[w * h];
+
+            for(int i=0; i<10; i++) {
+                completionService.submit(new Runner(i * part, (i + 1) * part, w, resultVertical, pixels, filterDelute));
+            }
+
+            int received = 0;
+            while(received < 10) {
+                completionService.take();
+                received++;
+            }
+
+            fixedPool.shutdown(); // shut down
+
+            //int[] resultVertical = runFilter(w, h, pixels, filterDelute);
+
+
+
+            System.out.println("Time " + (System.currentTimeMillis() - start));
+            grayscaleImg.getRaster().setPixels(0, 0, w, h, resultVertical);
+
+            ImageIO.write(grayscaleImg, "PNG", new File("java-result-multi.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/ea/gpu/Runner.java
@@ -0,0 +1,50 @@
+package org.ea.gpu;
+
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+
+public class Runner implements Callable<Integer> {
+    int sy, eh, w;
+    int[] newImage = null;
+    int[] pixels = null;
+    int[] filter = null;
+
+
+    public static final int FILTER_SIZE = 3;
+
+    public static int[] apply3x3Filter(int imageX, int imageY, int imageWidth, int[] img, int[] filter, int[] newImage) {
+
+        float sum = 0f;
+        for(int j=0; j<1000000; j++) {
+            sum = 0f;
+            for (int filterY = 0; filterY < FILTER_SIZE; filterY++) {
+                for (int filterX = 0; filterX < FILTER_SIZE; filterX++) {
+                    sum += img[((imageY + filterY) * imageWidth) + imageX + filterX] * filter[(filterY * FILTER_SIZE) + filterX];
+                }
+            }
+        }
+
+        newImage[((imageY + 1) * imageWidth) + imageX + 1] = (int)Math.floor(sum / 16f);
+
+        return newImage;
+    }
+
+    public Runner(int sy, int eh, int w, int[] image, int[] pixels, int[] filter) {
+        this.newImage = image;
+        this.pixels = pixels;
+        this.filter = filter;
+        this.sy = sy;
+        this.eh = eh;
+        this.w = w;
+        Arrays.fill(newImage, 0);
+    }
+
+    public Integer call() {
+        for(int y=sy; y<eh-FILTER_SIZE-1; y++) {
+            for(int x=0; x<w-FILTER_SIZE-1; x++) {
+                newImage = apply3x3Filter(x, y, w, pixels, filter, newImage);
+            }
+        }
+        return 1;
+    }
+}
